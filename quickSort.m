29738ccb820be06c6fa25c0eacef0bcf68a4f2a9
Lydia <lydia@lydia.(none)>
2014-04-27 19:56:51 +0200

    Vector aleatorio en octave

---
+++ quickSort.m
@@ -0,0 +1,4 @@
+%Algoritmo de ordenación quick sort a partir de un vector de tamaño n y datos aleatorios
+disp('Este programa ordena un vector de tamaño n, con numeros aleatorios.');
+n = input('Introduce el valor de n: ');
+vector=rand(n,1)*500;			%rellena el vector de tamaño n con numeros aleatorios entre cero y uno y los multiplica por 500
