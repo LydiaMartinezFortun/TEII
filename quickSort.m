7f233c4246033e4c816a9cfd7ec3f7206f606718
Lydia <lydia@lydia.(none)>
2014-04-28 13:57:30 +0200

    Añadido fichero latex

--- quickSort.m
+++ quickSort.m
@@ -1,9 +1,8 @@
 %Algoritmo de ordenación quick sort a partir de un vector de tamaño n y datos aleatorios
-disp('Este programa ordena un vector de tamaño n, con numeros aleatorios.');
-global n = input('Introduce el valor de n: ');
-global vector=(rand(n,1)*500);			%rellena el vector de tamaño n con numeros aleatorios entre cero y uno y los multiplica por 500
-disp('El vector es el siguiente: ');
-printf(' %f ',vector);
+%disp('Este programa ordena un vector de tamaño n, con numeros aleatorios.');
+global vector;%=(rand(n,1)*500);			%rellena el vector de tamaño n con numeros aleatorios entre cero y uno y los multiplica por 500
+%disp('El vector es el siguiente: ');
+%printf(' %f ',vector);
 
 function vectorOrdenado=quickSort(v)     
   vectorOrdenado = v;               
@@ -17,8 +16,28 @@ function vectorOrdenado=quickSort(v)
   end
 endfunction
  
-tic,u=quickSort(vector); t=toc
-disp('El vector ordenado es el siguiente: ');
+function tiemposTam=tiempoQuickSort(maxTam,incremento)
+	tiemposTam=zeros(1,0);
+	incremento = input('Introduce el valor de incremento: ');
+	maxTam = input('Introduce el valor del tamaño maximo: ');
+	tam=1;
+	while(tam<maxTam)
+		vector=(rand(tam,1)*500);			%rellena el vector de tamaño n con numeros aleatorios entre cero y uno y los multiplica por 500
+		%disp('El vector es el siguiente: ');
+		%printf(' %f ',vector);
+		tic,u=quickSort(vector); 
+		tiemposTam(tam)=toc;
 
-printf(' %f ',u)
-t
+		%disp('El vector ordenado es el siguiente: ');
+		%printf(' %f ',u);
+		%printf('\n');
+		%printf('El tiempo de ejecion con tamaño %d es: %f ',tam,tiemposTam(tam));
+		tam=tam+incremento;
+		endwhile
+
+endfunction
+
+tiempos=tiempoQuickSort(100,10);
+%printf('El tiempo de ejecion con tamaño : %f ',tiempos);
+%printf('\n');
+plot(tiempos);
